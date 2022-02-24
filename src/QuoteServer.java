15897fbaf08838078d9e5ee384c60aa42b10e3cb
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-02-23 16:27:11 -0800

    worked on QuoteServer

--- src/QuoteServer.java
+++ src/QuoteServer.java
@@ -8,5 +8,35 @@ import java.io.*;
 
 public class QuoteServer
 {
+    public static void main(String[] args) {
+        try{
+            ServerSocket sock = new ServerSocket(6017);
+
+            /*now listen for connections */
+
+            while(true){
+                Socket client = sock.accept(); //trying to accept the port 6017
+
+                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+
+
+                /* write the Date to the socket */
+
+                pout.println("this project aint bussin man....");
+
+
+                /*close the socket and resume */
+                /* listen for connection */
+
+                client.close();
+
+
+            }
+        }
+
+        catch(IOException ioe){
+            System.err.println(ioe);
+        }
+    }
 
 }
