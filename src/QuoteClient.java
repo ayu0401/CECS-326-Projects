687171b2c816d80c6339e0488fd2d39bbc5609f0
Aaron Yu <48599146+ayu0401@users.noreply.github.com>
2022-02-24 17:49:07 -0800

    Made Problem 1 work

--- src/QuoteClient.java
+++ src/QuoteClient.java
@@ -10,6 +10,21 @@ public class QuoteClient
 {
 	//Your code is here
     public static void main(String[] args) {
+        try{
+            //creates the socket so we can connect to the port that the server has opened
+            Socket socket = new Socket("127.0.0.1", 6017);
+
+            //get the input that was put into the socket
+
+        }
+        catch(UnknownHostException err){
+            System.out.println(err);
+
+        }
+
+        catch(IOException ioe){
+            System.out.println(ioe);
+        }
 
     }
 }
